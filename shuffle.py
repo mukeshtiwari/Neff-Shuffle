7dee99d11eb1350d48e1c0fea0b3798ca66ec367
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2025-04-17 21:30:33 +0100

    Simple k-shuffle but it is not working yet

--- shuffle.py
+++ shuffle.py
@@ -26,7 +26,7 @@ def generate_parameters(bits):
 
 
 # Prover knows: x_i = log_g X_i, y_i = log_g Y_i
-def ilmpp_prover(p : int, q : int, X : List[int], Y : List[int], x : List[int], y : List[int], gamma : int) -> Tuple[List[int], List[int]] :
+def ilmpp_proof(p : int, q : int, X : List[int], Y : List[int], x : List[int], y : List[int], gamma : int) -> Tuple[List[int], List[int]] :
   k = len(X)
   assert len(Y) == k
   assert len(x) == k
@@ -55,7 +55,7 @@ def ilmpp_prover(p : int, q : int, X : List[int], Y : List[int], x : List[int], 
   return A, r
   
 
-def ilmpp_verifier(p : int,  X : List[int], Y : List[int], A : List[int], r : List[int], gamma) -> bool: 
+def ilmpp_verification(p : int,  X : List[int], Y : List[int], A : List[int], r : List[int], gamma) -> bool: 
   k = len(X)
   assert len(Y) == k
   assert len(A) == k
@@ -91,11 +91,77 @@ y[8] *= h
 X = [pow(g, x[i], p) for i in range(N)]
 Y = [pow(g, y[i], p) for i in range(N)]
 gamma = getRandomRange(1, q)
-A, r = ilmpp_prover(p, q, X, Y, x, y, gamma)
-#print(x, y, X, Y, A, r)
-print(ilmpp_verifier(p, X, Y, A, r, gamma))
+A, r = ilmpp_proof(p, q, X, Y, x, y, gamma)
+print(ilmpp_verification(p, X, Y, A, r, gamma))
 
 
 
 # simple k-shuffle protocol 
+# It is not working yet but commiting it before going to bed 
+
+# Commitment function
+def commitment(p : int, g : int, c : int) -> int : 
+  C = mod_exp(g, c, p)  # C = g^c
+  return C
+
+
+# Computes X_hat = X * U^-1 mod p 
+def compute_hat(p : int, X : List[int], U : int) -> List[int]:
+  k = len(X)
+  X_hat = [(X[i] * mod_exp(U, -1, p)) % p for i in range(k)]
+  return X_hat
+
+
+# Two sequences of k elements of Zp, X1, . . . , Xk , and Y1, . . . , Yk are publicly known. The
+# prover, P, also knows xi = log_{g} Xi and yi = log_{g} Yi, but these are unknown to the verifier, V. 
+# In addition, constants c and d in Zq are known only to P, but commitments C = g^c
+# and D = g^d are made public. P is required to convince V that there is some permutation, π ∈ Σk , with the property
+# that Y_{i}^d = X_{π(i)}^c for all i ∈ {1, . . . , k}.
+
+
+def simple_k_shuffle_proof(p : int, X : List[int], Y : List[int], x : List [int], y : List [int], c : int, d : int, t : int, gamma : int) -> Tuple[List[int], List[int]] : 
+  k = len(X)
+  assert len(Y) == k
+  assert len(x) == k
+  assert len(y) == k
+
+  # 1 # V generates a random t ∈ Zq and gives it to P as a challenge.
+  # In practice, we should use Fiat-Shamir heuristic to generate the challenge
+  # and here we have passed it as a parameter
+  # t = getRandomRange(1, q)
+
+  C = commitment(p, g, c)  # C = g^c
+  D = commitment(p, g, d)  # D = g^d
+
+  # 2 # P computes U = D^t, W = C^t, X_hat = X * U^-1, Y_hat = Y * W^-1
+  U = commitment(p, D, t)  # U = D^t mod p 
+  W = commitment(p, C, t) # W = C^t mod p
+  X_hat = compute_hat(p, X, U) # X_hat = X * U^-1 mod p
+  Y_hat = compute_hat(p, Y, W) # Y_hat = Y * W^-1 mod p
+
+  # 3 P and V execute the SILPP for the two length 2k vectors
+  Xret = X_hat + [C] * k 
+  Yret = Y_hat + [D] * k
+  xret = x + [c] * k
+  yret = y + [d] * k
+  A, r = ilmpp_proof(p, q, Xret, Yret, xret, yret, gamma)
+
+  return Xret, Yret, A, r 
+
+def simple_k_shuffle_verification(p : int, X : List[int], Y : List[int], A : List[int], r : List[int], gamma : int) -> bool:
+  return ilmpp_verification(p, X, Y, A, r, gamma) 
+
+#Testing code
+p, q, g = generate_parameters(100)
+N = 100
+x = [getRandomRange(1, q) for _ in range(N)]
+permutation = [i for i in range(N)] 
+y = [x[permutation[i]] for i in range(N)]
+X = [pow(g, x[i], p) for i in range(N)]
+Y = [pow(g, y[i], p) for i in range(N)]
+c, d = (getRandomRange(1, q) for _ in range(2))
+t = getRandomRange(1, q)
+gamma = getRandomRange(1, q)
+Xret, Yret, A, r = simple_k_shuffle_proof(p, X, Y, x, y, c, d, t, gamma)
+print(simple_k_shuffle_verification(p, Xret, Yret, A, r, gamma))
 
